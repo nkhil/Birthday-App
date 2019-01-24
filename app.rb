76b8ac4b9c31455027c615987f2e9299f705ba9f
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-24 11:09:51 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra/base'
 
 class Birthday < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/birthday' do
+    session[:name] = params[:name]
+    session[:date] = params[:date]
+    session[:month] = params[:month]
+    
+    redirect '/greet'
+  end
+
+  get '/greet' do
+    @name = session[:name]
+    @date = session[:date]
+    @month = session[:month]
+    @time = Time.new
+    @month = @time.month
+    @day = @time.day
+    erb :greet
   end
 
   run! if app_file == $0
